a18c3f31cd4667771d19028932de4e04a2bcc1e2
wiliender <wiliendersilva@gmail.com>
2024-02-11 23:33:40 -0300

    feat(rotas):sparkles: Adiciona rotas para CRUD de usuários

---
+++ src/router.go/rotas/usuarios.go
@@ -0,0 +1,46 @@
+package rotas
+
+import "net/http"
+
+var rotasUsuarios = []Rota{
+	{
+		URI:    "/usuarios",
+		Metodo: http.MethodPost,
+		Funcao: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequerAutenticacao: false,
+	},
+	{
+		URI:    "/usuarios",
+		Metodo: http.MethodGet,
+		Funcao: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequerAutenticacao: false,
+	},
+	{
+		URI:    "/usuarios/{usuarioID}",
+		Metodo: http.MethodGet,
+		Funcao: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequerAutenticacao: false,
+	},
+	{
+		URI:    "/usuarios/{usuarioID}",
+		Metodo: http.MethodPut,
+		Funcao: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequerAutenticacao: false,
+	},
+	{
+		URI:    "/usuarios/{usuarioID}",
+		Metodo: http.MethodDelete,
+		Funcao: func(w http.ResponseWriter, r *http.Request) {
+
+		},
+		RequerAutenticacao: false,
+	},
+}
