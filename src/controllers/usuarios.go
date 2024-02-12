04dd238a11c3677deb7e193cf671985a2753b931
wiliender <wiliendersilva@gmail.com>
2024-02-11 23:43:30 -0300

    fix

--- src/controllers/usuarios.go
+++ src/controllers/usuarios.go
@@ -3,26 +3,26 @@ package controllers
 import "net/http"
 
 // CriarUsuario insere um usuario na base de dados
-func CriarUsuario(w http.ResponseWriter, r *http.Response) {
+func CriarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Criando Usuário!"))
 }
 
 // BuscarUsuarios busca todos os usuarios na base de dados
-func BuscarUsuarios(w http.ResponseWriter, r *http.Response) {
+func BuscarUsuarios(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscando todos os Usuário!"))
 }
 
 // BuscarUsuario busca um unico usuario na base de dados
-func BuscarUsuario(w http.ResponseWriter, r *http.Response) {
+func BuscarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Buscando um unico Usuário!"))
 }
 
 // AtualizarUsuario atualiza um usuario na base de dados
-func AtualizarUsuario(w http.ResponseWriter, r *http.Response) {
+func AtualizarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Atualizando Usuário!"))
 }
 
 // DeletarUsuario deleta um usuario na base de dados
-func DeletarUsuario(w http.ResponseWriter, r *http.Response) {
+func DeletarUsuario(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Deletando Usuário!"))
 }
